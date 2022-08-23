9347726be4bf253145466a20a189773e2dd0fd94
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2022-08-23 13:54:05 -0400

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+function Home() {
+  return (
+    <>
+      <p>Home</p>
+    </>
+  )
+}
+
+function Info() {
+  return (
+    <>
+      <p>Info</p>
+    </>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <BrowserRouter>
+        <div>
+          <h1>Home</h1>
+          <nav>
+            <Link to="/">Home</Link>
+            <br/>
+            <Link to="/info">info</Link>
+          </nav>
+        </div>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/info" element={<Info />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+
   )
 }
 
